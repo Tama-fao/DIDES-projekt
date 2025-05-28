a620c8daad54c4edb7b8a2e4bf242d9cc1b3656d
HabbaTehJutt <mira.cavelti@stud.hslu.ch>
2025-05-28 15:10:53 +0200

    phone 5 neu animationen

--- visual novel/phone5.js
+++ visual novel/phone5.js
@@ -92,12 +92,43 @@ function triggerFlyIns() {
     setTimeout(() => arme.classList.add("fly-out"), 2000);
   }, 25000);
 
-   setTimeout(() => {
+  // Neue Slide-In Animation starten 500ms nach Arme-Out (ca. 27,5s)
+  setTimeout(() => {
+    triggerSlideIn();
+  }, 27500);
+
+  setTimeout(() => {
     laurie.classList.add("fly-in");
     setTimeout(() => laurie.classList.add("fly-out"), 1000);
   }, 100);
 
   startMorphSequence();
+}
+
+function triggerSlideIn() {
+  const left1 = document.getElementById("slide-left1");
+  const left2 = document.getElementById("slide-left2");
+  const right1 = document.getElementById("slide-right1");
+  const right2 = document.getElementById("slide-right2");
+
+  [left1, left2, right1, right2].forEach(img => {
+    img.style.display = "block";
+    img.classList.remove("stockend-slide-left", "stockend-slide-right");
+    img.style.opacity = "1";
+  });
+
+  // Animationen hinzufügen
+  left1.classList.add("stockend-slide-left");
+  left2.classList.add("stockend-slide-left");
+  right1.classList.add("stockend-slide-right");
+  right2.classList.add("stockend-slide-right");
+
+  // Optional: nach Ende Animation wieder ausblenden
+  setTimeout(() => {
+    [left1, left2, right1, right2].forEach(img => {
+      img.style.display = "none";
+    });
+  }, 9000); // Dauer der Animation in ms
 }
 
 function startMorphSequence() {
@@ -111,41 +142,56 @@ function startMorphSequence() {
     document.getElementById("morph7")
   ];
 
-  let currentIndex = 0;
+  morphFrames.forEach((img, i) => {
+    img.style.top = "50%";
+    img.style.left = "50%";
+    img.style.transformOrigin = "center center";
+    img.style.transform = "translate(-50%, -50%) rotate(90deg)";
 
-  // Vorbereitung: Alle verstecken & 90° drehen (außer das erste)
-  morphFrames.forEach((img, index) => {
-    img.style.display = 'none';
-    if (index !== 0) {
-      img.classList.add('rotated-90');
+    if (i === 0) {
+      img.classList.add("stockend-animation");
+      img.style.display = "block";
+    } else {
+      img.classList.remove("stockend-animation");
+      img.style.display = "none";
     }
   });
 
-  // Erstes Bild zeigen & rotieren
-  const first = morphFrames[0];
-  first.style.display = 'block';
-  first.classList.add('rotate-stockend');
-
-  // Nach Drehung: weitere Bilder zeigen
-  setTimeout(() => {
-    nextFrame();
-  }, 1000); // Warte bis Animation vorbei
+  let currentIndex = 0;
 
   function nextFrame() {
     if (currentIndex < morphFrames.length - 1) {
-      morphFrames[currentIndex].style.display = 'none';
+      if (currentIndex === 0) {
+        morphFrames[0].classList.remove("stockend-animation");
+        morphFrames[0].style.display = "none";
+      } else {
+        morphFrames[currentIndex].style.display = "none";
+      }
+
       currentIndex++;
-      morphFrames[currentIndex].style.display = 'block';
+      morphFrames[currentIndex].style.display = "block";
+      morphFrames[currentIndex].style.transform = "translate(-50%, -50%) rotate(90deg)";
       setTimeout(nextFrame, 1000);
     } else {
-      // Optional: am Ende ausblenden
+      morphFrames.forEach(img => {
+        img.style.display = "none";
+        img.classList.remove("stockend-animation");
+        img.classList.remove("stockend-slide-out");
+      });
+
+      const lastFrame = morphFrames[morphFrames.length - 1];
+      lastFrame.style.display = "block";
+      lastFrame.classList.add("stockend-animation");
+
       setTimeout(() => {
-        morphFrames.forEach(img => img.style.display = 'none');
-      }, 1000);
+        lastFrame.classList.remove("stockend-animation");
+        lastFrame.classList.add("stockend-slide-out");
+      }, 2000);
     }
   }
-}
 
+  setTimeout(nextFrame, 2000);
+}
 
 
 
